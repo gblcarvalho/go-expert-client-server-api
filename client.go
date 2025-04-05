25e5b3f0eec665dcb9c87f295c9c4629a5f0fe84
Gabriel Miranda Carvalho <gabrielm.car@gmail.com>
2025-04-05 13:29:00 -0300

    terminando client

--- client.go
+++ client.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"io"
 	"net/http"
 	"os"
 	"time"
 )
 
+type DollarPriceRcv struct {
+	Bid string `json:"bid"`
+}
+
 func main() {
+	f, err := os.Create("cotacao.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 	ctx, cancel := context.WithTimeout(context.Background(), 300 * time.Millisecond)
 	defer cancel()
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
@@ -20,6 +30,15 @@ func main() {
 		panic(err)
 	}
 	defer res.Body.Close()
-	io.Copy(os.Stdout, res.Body)
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	var price DollarPriceRcv
+	err = json.Unmarshal(body, &price)
+	if err != nil {
+		panic(err)
+	}
+	f.Write([]byte("Dólar: " + price.Bid))
 }
 
